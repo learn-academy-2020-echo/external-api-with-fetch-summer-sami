a20a4ce2006d1f18a027d96935cb15202bcf5fe0
summer-cook <summercook@bellsouth.net>
2021-01-10 20:56:07 -0500

    refactor with single DogPic component

--- dog-randomizer/src/App.js
+++ dog-randomizer/src/App.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react'
-import GermanShepherd from './components/GermanShepherd';
-import AustralianShepherd from './components/AustralianShepherd';
-import FrenchBulldog from './components/FrenchBulldog';
+import DogPic from './components/DogPic';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      pic: ""
+      dog: {
+        frenchBulldog: {
+          url: "https://dog.ceo/api/breed/bulldog/images/random",
+          name: "Frenchies"
+        },
+        germanShepherd: {
+          url: "https://dog.ceo/api/breed/germanshepherd/images/random",
+          name: "German Shepherds"
+        },
+        rottweiler: {
+          url: "https://dog.ceo/api/breed/rottweiler/images/random",
+          name: "Rottweilers"
+        },
+        goldenRetriever: {
+          url: "https://dog.ceo/api/breed/retriever/golden/images/random",
+          name: "Golden Retrievers"
+        },
+        samoyed: {
+          url: "https://dog.ceo/api/breed/samoyed/images/random",
+          name: "Samoyeds"
+        },
+        husky: {
+          url: "https://dog.ceo/api/breed/husky/images/random",
+          name: "Huskies"
+        },
+        borderCollie: {
+          url: "https://dog.ceo/api/breed/collie/border/images/random",
+          name: "Border Collies"
+        },   
+        pitbull: {
+          url: "https://dog.ceo/api/breed/pitbull/images/random",
+          name: "Pittbulls"
+        },     
+        mutts: {
+          url: "https://dog.ceo/api/breed/mix/images/random",
+          name: "Mutts"
+        },
+      } 
     }
   }
 
-
-  testFun = (e) => {
-    console.log(e)
-
-  }
-
-  getDogPic = (e) => {
-  fetch(e)
-    .then(response => {
-      return response.json()
-    })
-    .then(payload => {
-      this.setState({ pic: payload.message})
-    })
-    //if there is an issue and the promise is rejected, the function will be able to handle the errors. ending each fetch request with a catch is good practice
-    .catch(error => {
-      console.log(error)
-    })
-  }
-  componentDidMount(){
-    this.getDogPic("https://dog.ceo/api/breed/bulldog/images/random")
-  }
-
-  getDogOnClick = () => {
-    this.getDogPic("https://dog.ceo/api/breed/bulldog/images/random")
-  }
 
   render(){
     return (
@@ -45,36 +54,49 @@ class App extends Component {
       <h1>Dog Randomizer</h1>
 
       <div style={{display: "inline-block"}}>
-      <GermanShepherd />
-      <AustralianShepherd />
-      <GermanShepherd />
-      </div>
-      <div style={{display: "inline-block"}}>
-      <GermanShepherd />
-      <AustralianShepherd />
-      <GermanShepherd />
-      </div>
-      <div style={{display: "inline-block"}}>
-      <GermanShepherd />
-      <AustralianShepherd />
-      <GermanShepherd />
-      </div>
-
-      <FrenchBulldog 
-      getDogPic={ this.getDogPic} 
-      componentDidMount={ this.componentDidMount }
-      getDogOnClick={ this.getDogOnClick }
-      testFun={ this.testFun}
+      <DogPic 
+        dogURL= {this.state.dog.frenchBulldog.url}
+        dogName= {this.state.dog.frenchBulldog.name}
       />
-
-
-      {/* { this.state.pic &&
-      <div> 
-      <img onClick= { this.getDogOnClick }
-      src= { this.state.pic } style= {{ height: '200px' }}
-      alt='bulldogs' />
+       <DogPic 
+        dogURL= {this.state.dog.germanShepherd.url}
+        dogName= {this.state.dog.germanShepherd.name}
+      />
+       <DogPic 
+        dogURL= {this.state.dog.rottweiler.url}
+        dogName= {this.state.dog.rottweiler.name}
+      />
       </div>
-  } */}
+      <div style={{display: "inline-block"}}>
+      <DogPic 
+        dogURL= {this.state.dog.goldenRetriever.url}
+        dogName= {this.state.dog.goldenRetriever.name}
+      />
+       <DogPic 
+        dogURL= {this.state.dog.samoyed.url}
+        dogName= {this.state.dog.samoyed.name}
+      />
+       <DogPic 
+        dogURL= {this.state.dog.husky.url}
+        dogName= {this.state.dog.husky.name}
+      />
+      </div>
+
+      <div style={{display: "inline-block"}}>
+      <DogPic 
+        dogURL= {this.state.dog.borderCollie.url}
+        dogName= {this.state.dog.borderCollie.name}
+      />
+       <DogPic 
+        dogURL= {this.state.dog.pitbull.url}
+        dogName= {this.state.dog.pitbull.name}
+      />
+       <DogPic 
+        dogURL= {this.state.dog.mutts.url}
+        dogName= {this.state.dog.mutts.name}
+      />
+      </div>
+
       </center>
       </div>
     )
