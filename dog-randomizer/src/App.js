356b110775dc99c6f8d4425946ac9b16125c939a
summer-cook <summercook@bellsouth.net>
2021-01-11 20:37:38 -0500

    adds a separate file for dog array

--- dog-randomizer/src/App.js
+++ dog-randomizer/src/App.js
@@ -1,48 +1,12 @@
 import React, { Component } from 'react'
 import DogPic from './components/DogPic';
+import dogs from './dogArray.js';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      dog: {
-        frenchBulldog: {
-          url: "https://dog.ceo/api/breed/bulldog/images/random",
-          name: "Frenchies"
-        },
-        germanShepherd: {
-          url: "https://dog.ceo/api/breed/germanshepherd/images/random",
-          name: "German Shepherds"
-        },
-        rottweiler: {
-          url: "https://dog.ceo/api/breed/rottweiler/images/random",
-          name: "Rottweilers"
-        },
-        goldenRetriever: {
-          url: "https://dog.ceo/api/breed/retriever/golden/images/random",
-          name: "Golden Retrievers"
-        },
-        samoyed: {
-          url: "https://dog.ceo/api/breed/samoyed/images/random",
-          name: "Samoyeds"
-        },
-        husky: {
-          url: "https://dog.ceo/api/breed/husky/images/random",
-          name: "Huskies"
-        },
-        borderCollie: {
-          url: "https://dog.ceo/api/breed/collie/border/images/random",
-          name: "Border Collies"
-        },   
-        pitbull: {
-          url: "https://dog.ceo/api/breed/pitbull/images/random",
-          name: "Pittbulls"
-        },     
-        mutts: {
-          url: "https://dog.ceo/api/breed/mix/images/random",
-          name: "Mutts"
-        },
-      } 
+      dog: dogs
     }
   }
 
